fba88cfe81033568a320908902ff8d8692c762a8
Vincent Saïsset <vincent.saisset@gmail.com>
2015-09-01 09:58:56 +0200

    Dashboard update

--- setup/admin/dashboard.php
+++ setup/admin/dashboard.php
@@ -66,9 +66,9 @@ function extra_dashboard_widgets() {
 	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
 	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
-	unset($wp_meta_boxes['dashboard']['side']['high']['id']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 }
-add_action('wp_dashboard_setup', 'extra_dashboard_widgets', 999);
+add_action('wp_dashboard_setup', 'extra_dashboard_widgets');
 remove_action( 'welcome_panel', 'wp_welcome_panel' );
 /**********************
  *

56865a717d0e71a232738041bfd62428163553cb
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2015-11-17 16:10:40 +0100

    Added extra.slider as a module

--- setup/scripts.php
+++ setup/scripts.php
@@ -37,8 +37,6 @@ function extra_template_enqueue_scripts() {
         'mobile' => 'only screen and (max-width: 690px)'
 	));
 	wp_localize_script('extra', 'extraResponsiveSizes', $sizes);
-	// EXTRA-SLIDER
-	wp_enqueue_script('extra-slider', EXTRA_URI . '/assets/js/lib/extra.slider.js', array('jquery', 'extra', 'tweenmax'), null, true);
 	// EXTRA
 	wp_enqueue_script('extra-gallery', EXTRA_URI . '/assets/js/lib/extra.gallery.js', array('jquery', 'extra-slider', 'tweenmax'), null, true);
 	// TO COPY IN THEME scripts.php IF NEEDED

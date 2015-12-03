3f1dbf6f6f3a5ce373fe9a6134e7767ab75d90f7
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2015-12-03 12:11:28 +0100

    Changed the hook to enqueue extra slider

--- modules/extra.slider/setup.php
+++ modules/extra.slider/setup.php
@@ -12,4 +12,4 @@ function extra_slider_init () {
 	wp_enqueue_script('extra.slider', EXTRA_MODULES_URI.'/extra.slider/inc/js/extra.slider.js', array('jquery'), null, true);
 	wp_enqueue_style('extra.slider', EXTRA_MODULES_URI.'/extra.slider/inc/css/extra.slider.css', array(), false, 'all');
 }
-add_action('init', 'extra_slider_init');
+add_action('wp_enqueue_scripts', 'extra_slider_init');

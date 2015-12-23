7fbf75dd401fc12858ded811516dfeb779c006c0
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2015-12-23 15:32:24 +0100

    Revert "Page builder / Accordion / Anchor"
    This reverts commit b4aabdf28d617cdc165ec27001f8c03fe99537ef.

--- includes/extra-metabox/page-builder/blocks/Accordion/js/accordion-front.js
+++ includes/extra-metabox/page-builder/blocks/Accordion/js/accordion-front.js
@@ -6,38 +6,18 @@ $(document).ready(function(){
 	 *********************/
 	$(".accordeon-element").each(function(){
 		var wrapper = $(this);
-		var title = wrapper.find(" > .tab-title a");
+		var title = wrapper.find(" > .tab-title");
 		var content = wrapper.find(" > .tab-content");
 		var height = content.innerHeight();
 		content.height(0);
-		title.bind("click", function(event){
-			event.preventDefault();
+		title.bind("click", function(){
 			if(content.height() > 0) {
 				wrapper.removeClass("open");
 				TweenMax.to(content, 0.3, {css:{height:0}});
 			} else {
 				wrapper.addClass("open");
 				TweenMax.to(content, 0.3, {css:{height:height}});
-				window.location.hash = '/'+$(this).attr('id');
 			}
 		});
-	});
-
-	$window.on('load', function () {
-		var hash = window.location.hash;
-
-		if (hash != null && hash != '') {
-			hash = hash.substr(2);
-			var $trigger = $('#'+hash);
-
-			if ($trigger.size() > 0) {
-				$trigger.trigger('click');
-				TweenMax.to($window, 0, {
-					scrollTo: {
-						y: $trigger.offset().top - 150
-					}
-				});
-			}
-		}
 	});
 });

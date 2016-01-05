06e82181dad0b2fff6f923894cfabec5369d8ff5
Vincent Saïsset <vincent.saisset@gmail.com>
2016-01-05 14:28:34 +0100

    Add tweenMax in dependencies for scripts

--- modules/extra.menu/setup.php
+++ modules/extra.menu/setup.php
@@ -9,7 +9,7 @@
  *
  *********************/
 function extra_menu_init () {
-	wp_enqueue_script('extra.menu', EXTRA_MODULES_URI.'/extra.menu/inc/js/extra.menu.js', array('jquery'), null, true);
+	wp_enqueue_script('extra.menu', EXTRA_MODULES_URI.'/extra.menu/inc/js/extra.menu.js', array('jquery', 'tweenmax'), null, true);
 	wp_enqueue_style('extra.menu', EXTRA_MODULES_URI.'/extra.menu/inc/css/extra.menu.css', array(), false, 'all');
 }
 add_action('init', 'extra_menu_init');

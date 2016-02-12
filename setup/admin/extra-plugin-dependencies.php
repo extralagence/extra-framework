5bf0f21e6219351f6937825d815733125cad0d0c
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-02-12 15:06:36 +0100

    Added contact form 7 as a dependancie

--- setup/admin/extra-plugin-dependencies.php
+++ setup/admin/extra-plugin-dependencies.php
@@ -101,6 +101,11 @@ function extra_register_required_plugins() {
 			'slug'      => 'redux-framework',
 			'required'  => true
 		),
+		array(
+			'name'      => 'Contact Form 7',
+			'slug'      => 'contact-form-7',
+			'required'  => true
+		)
 
 		// This is an example of the use of 'is_callable' functionality. A user could - for instance -
 		// have WPSEO installed *or* WPSEO Premium. The slug would in that last case be different, i.e.

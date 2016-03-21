36222e0a2e4f487de766f2436578f865ac1beee6
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-03-21 17:05:13 +0100

    Updated extra scroll animator slug

--- modules/extra.scrollanimator/setup.php
+++ modules/extra.scrollanimator/setup.php
@@ -13,6 +13,6 @@ function extra_scroll_animator_init () {
 	if(!$extra_enabled_extra_scroll_animator) {
 		return;
 	}
-	wp_enqueue_script('extra.scrollanimator', EXTRA_MODULES_URI.'/extra.scrollanimator/inc/js/extra.scrollanimator.js', array('jquery', 'tweenmax', 'extra'), null, true);
+	wp_enqueue_script('extra-scroll-animator', EXTRA_MODULES_URI.'/extra.scrollanimator/inc/js/extra.scrollanimator.js', array('jquery', 'tweenmax', 'extra'), null, true);
 }
 add_action('wp_enqueue_scripts', 'extra_scroll_animator_init');

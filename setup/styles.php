f7630680eff5924f5c30e7880f656d8177cf6f3a
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-03-21 14:50:41 +0100

    Added fancybox as a submodule

--- setup/styles.php
+++ setup/styles.php
@@ -11,8 +11,6 @@
 function extra_template_enqueue_styles() {
 	// REMOVE CSS FROM CONTACT FORM 7
 	add_filter( 'wpcf7_load_css', '__return_false' );
-	// FANCYBOX
-	wp_enqueue_style( 'fancybox', EXTRA_URI . '/assets/css/jquery.fancybox.css', array(), false, 'all' );
 	// FANCY SELECT
 //	wp_enqueue_style( 'fancy-select', EXTRA_URI . '/assets/css/fancyselect.css', array(), false, 'all' );
 	// EXTRA MOSAIC

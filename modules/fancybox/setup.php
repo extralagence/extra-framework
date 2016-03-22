4b8a86667dffbf9400562c6cc6062acd04327b8b
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-03-22 14:19:31 +0100

    Fixed js dependancies

--- modules/fancybox/setup.php
+++ modules/fancybox/setup.php
@@ -13,11 +13,11 @@ function extra_fancybox_init() {
 	if ( !$extra_enabled_fancybox ) {
 		return;
 	}
-	wp_enqueue_script( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/source/jquery.fancybox.pack.js', array( 'jquery', 'extra' ), null, true );
+	wp_enqueue_script( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/source/jquery.fancybox.pack.js', array( 'jquery' ), null, true );
 	wp_enqueue_script( 'mousewheel', EXTRA_MODULES_URI . '/fancybox/inc/lib/jquery.mousewheel.pack.js', array( 'fancybox' ), null, true );
 	wp_enqueue_script( 'extra.fancybox', EXTRA_MODULES_URI . '/fancybox/js/fancybox.js', array( 'fancybox' ), null, true );
 
-	wp_enqueue_style( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/source/jquery.fancybox.css', array(), false, 'all' );
+	wp_enqueue_style( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/source/jquery.fancybox.css', array('fanxybox', 'extra'), false, 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'extra_fancybox_init' );

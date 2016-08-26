afe7eb5a30ba343401d43591b4fbcc9d6dc88400
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-08-26 14:00:18 +0200

    Updated greensock libs

--- setup/scripts.php
+++ setup/scripts.php
@@ -11,11 +11,11 @@
 function extra_template_enqueue_scripts() {
 	// REPLACE JQUERY
 	wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js', null, null, apply_filters('extra_jquery_in_footer', true));
+	wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js', null, null, apply_filters(' ', true));
 	// TWEENMAX
-	wp_enqueue_script('tweenmax', EXTRA_URI . '/assets/js/lib/TweenMax.min.js', array('jquery'), null, true);
+	wp_enqueue_script('tweenmax', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js', array('jquery'), null, true);
 	// SCROLLTO
-	wp_enqueue_script('tweenmax-scrollto', EXTRA_URI . '/assets/js/lib/ScrollToPlugin.min.js', array('jquery'), null, true);
+	wp_enqueue_script('tweenmax-scrollto', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/plugins/ScrollToPlugin.min.js', array('jquery'), null, true);
 	// EXTRA
 	wp_enqueue_script('extra', EXTRA_URI . '/assets/js/lib/extra.js', array('jquery', 'tweenmax'), null, true);
 	// RESPONSIVE SIZES

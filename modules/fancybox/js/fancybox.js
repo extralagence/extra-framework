fe2f38278c84b8767e99fd50c0197c70f69f3d63
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-09-27 14:38:23 +0200

    Updated fancybox

--- modules/fancybox/js/fancybox.js
+++ modules/fancybox/js/fancybox.js
@@ -10,19 +10,25 @@ $window.on('extra:fancybox:init', function (event, $parent) {
 function extraInitFancybox($parent) {
 	$parent.find("a[href$='.jpg'], a[href$='.jpeg'], a[href$='.png'], a[href$='.gif'], a[href$='.svg'], .fancybox").not('.no-fancybox').filter(function () {
 		return $(this).attr("target") != "_blank";
-	}).attr("data-fancybox-group", "gallery").fancybox(defaultOptions.fancyboxOptions).each(function () {
+	}).attr("data-fancybox-group", "gallery").each(function () {
 		var $this = $(this),
 			$img = $this.find(" > img").first();
 		if ($img.length) {
 			$(this).addClass("zoom");
-			if ($img.hasClass("alignleft")) {
-				$this.addClass("alignleft");
-			}
-			if ($img.hasClass("alignright")) {
-				$this.addClass("alignright");
+		}
+		if ($this.next(".wp-caption-text").length) {
+			console.log(defaultOptions);
+			defaultOptions['fancyboxOptions']['beforeShow'] = function () {
+				this.title = $this.next(".wp-caption-text").html();
 			}
 		}
-	});
+		/*if ($img.hasClass("alignleft")) {
+		 $this.addClass("alignleft");
+		 }
+		 if ($img.hasClass("alignright")) {
+		 $this.addClass("alignright");
+		 }*/
+	}).fancybox(defaultOptions.fancyboxOptions);
 }
 /*********************
  *

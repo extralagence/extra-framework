4201702e77976dc76832c2f6ca3d025c23c31012
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-11-04 12:39:55 +0100

    💖 Minor custom share

--- modules/custom_share/js/custom_share.js
+++ modules/custom_share/js/custom_share.js
@@ -1,13 +1,26 @@
-$(document).ready(function() {
-	$(".extra-social-button").not('.extra-social-share').each(function() {
+$(document).ready(function () {
+
+	// AJAX SVG SPRITE
+	$.get(extra_custom_share_params.assets_uri + "img/sprite.svg", function (data) {
+		var div = document.createElement("div");
+		div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
+		document.body.insertBefore(div, document.body.childNodes[0]);
+		$("html").addClass("extra-custom-share-svg-loaded");
+	});
+
+	extraInitShare($('body'));
+
+});
+function extraInitShare($wrapper) {
+	$wrapper.find(".extra-social-button").not('.extra-social-share').each(function () {
 		var $this = $(this),
 			url = $this.data("url"),
 			counter = $this.data("counter");
 
 		if (counter) {
 			// console.log(counter);
-			$.getJSON(counter, function(data) {
-				if(data['count']) {
+			$.getJSON(counter, function (data) {
+				if (data['count']) {
 					$this.find('.counter').text(data['count']);
 				} else {
 					$.each(data, function (key, val) {
@@ -18,24 +31,14 @@ $(document).ready(function() {
 				}
 			});
 		}
-		$this.on('click', function(e) {
+		$this.on('click', function (e) {
 			e.preventDefault();
-			window.open($this.attr('href'),"Partage","menubar=no, status=no, scrollbars=no, menubar=no, width=600, height=500");
+			window.open($this.attr('href'), "Partage", "menubar=no, status=no, scrollbars=no, menubar=no, width=600, height=500");
 		});
 	});
 
-	$('.extra-social-share').fancybox({
-		margin: 0,
+	$wrapper.find('.extra-social-share').fancybox({
+		margin : 0,
 		padding: 0
 	});
-
-	// AJAX SVG SPRITE
-	$.get(extra_custom_share_params.assets_uri + "/img/sprite.svg", function (data) {
-		var div = document.createElement("div");
-		div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
-		document.body.insertBefore(div, document.body.childNodes[0]);
-		$("html").addClass("extra-custom-share-svg-loaded");
-	});
-
-
-});
+}

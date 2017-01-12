504ea976f340177fba4ce282a4a8c376dfc46d2e
Vincent Saïsset <vincent.saisset@gmail.com>
2017-01-12 16:19:57 +0100

    ✨ Add rewrite menu urls for redirection's page

--- modules/_redirection/setup.php
+++ modules/_redirection/setup.php
@@ -30,80 +30,113 @@ function extra_redirection__metabox() {
 add_action( 'init', 'extra_redirection__metabox', 12 );
 
 
-function extra_redirect() {
+function extra_get_redirect($page_id) {
+	global $redirection_metabox;
+	$data = $redirection_metabox->the_meta($page_id);
+
+	$redirection_type = ! empty( $data['redirection_type'] ) ? $data['redirection_type'] : 'auto';
+
+	$response = array(
+		'url' => null,
+		'error' => false
+	);
+
+	switch ( $redirection_type ) {
+		case 'auto' :
+			$redirect_params = array(
+				'child_of'    => $page_id,
+				'sort_column' => 'menu_order'
+			);
+			$redirect_params = apply_filters( 'extra_redirection_get_pages_params', $redirect_params );
+			$pagekids        = get_pages( $redirect_params );
+			if ( ! empty( $pagekids ) ) {
+				$firstchild = $pagekids[0];
+				$response['url'] = get_permalink( $firstchild->ID );
+			} else {
+				$response['error'] = __( "Cette page n'a pas d'enfant", "extra" );
+			}
+			break;
+
+		case 'manual' :
+
+			if ( ! empty( $data['redirection_manual'] ) ) {
+				$response['url'] = $data['redirection_manual'];
+			} else {
+				$response['error'] = __( "Cette page n'a pas d'url de destination", "extra" );
+			}
+			break;
+
+		case 'content' :
+
+			if ( ! empty( $data['redirection_content'] ) ) {
+				$response['url'] = get_permalink( $data['redirection_content'] );
+			} else {
+				$response['error'] = __( "Cette page n'a pas d'url de destination", "extra" );
+			}
+			break;
+
+		case 'post-type' :
+
+			$redirection_value = $data['redirection_post-type'];
+			if ( ! empty( $redirection_value ) ) {
+
+				$targets = get_posts( array(
+					'post_type'        => $redirection_value,
+					'numberposts'      => 1,
+					'orderby'          => 'menu_order',
+					'order'            => 'ASC',
+					'post_status'      => 'publish,private',
+					'suppress_filters' => 0
+				) );
+
+				if ( count( $targets ) > 0 ) {
+					if ( function_exists( 'icl_object_id' ) ) {
+						$target_id = icl_object_id( $targets[0]->ID, $redirection_value, true );
+					} else {
+						$target_id = $targets[0]->ID;
+					}
+
+					$response['url'] = get_permalink( $target_id );
+				} else {
+					$response['error'] = __( "Cette page n'a pas d'url de destination", "extra" );
+				}
+			} else {
+				$response['error'] = __( "Cette page n'a pas d'url de destination", "extra" );
+			}
+			break;
+	}
+
+	return $response;
+}
+
+add_action( 'template_redirect', function () {
 	if ( is_page_template( 'template-redirect.php' ) ) {
 		global $post, $redirection_metabox;
 		the_post();
-		$data = $redirection_metabox->the_meta();
+		$redirect = extra_get_redirect ($post->ID);
 
-		$redirection_type = ! empty( $data['redirection_type'] ) ? $data['redirection_type'] : 'auto';
-
-		switch ( $redirection_type ) {
-			case 'auto' :
-				$redirect_params = array(
-					'child_of'    => $post->ID,
-					'sort_column' => 'menu_order'
-				);
-				$redirect_params = apply_filters( 'extra_redirection_get_pages_params', $redirect_params );
-				$pagekids        = get_pages( $redirect_params );
-				if ( ! empty( $pagekids ) ) {
-					$firstchild = $pagekids[0];
-					wp_redirect( get_permalink( $firstchild->ID ) );
-				} else {
-					_e( "Cette page n'a pas d'enfant", "extra" );
-				}
-				break;
-
-			case 'manual' :
-
-				if ( ! empty( $data['redirection_manual'] ) ) {
-					wp_redirect( $data['redirection_manual'] );
-				} else {
-					_e( "Cette page n'a pas d'url de destination", "extra" );
-				}
-				break;
-
-			case 'content' :
-
-				if ( ! empty( $data['redirection_content'] ) ) {
-					wp_redirect( get_permalink( $data['redirection_content'] ) );
-				} else {
-					_e( "Cette page n'a pas d'url de destination", "extra" );
-				}
-				break;
-
-			case 'post-type' :
-
-				$redirection_value = $data['redirection_post-type'];
-				if ( ! empty( $redirection_value ) ) {
-
-					$targets = get_posts( array(
-						'post_type'        => $redirection_value,
-						'numberposts'      => 1,
-						'orderby'          => 'menu_order',
-						'order'            => 'ASC',
-						'post_status'      => 'publish,private',
-						'suppress_filters' => 0
-					) );
-
-					if ( count( $targets ) > 0 ) {
-						if ( function_exists( 'icl_object_id' ) ) {
-							$target_id = icl_object_id( $targets[0]->ID, $redirection_value, true );
-						} else {
-							$target_id = $targets[0]->ID;
-						}
-						wp_redirect( get_permalink( $target_id ) );
-					} else {
-						_e( "Cette page n'a pas d'url de destination", "extra" );
-					}
-				} else {
-					_e( "Cette page n'a pas d'url de destination", "extra" );
-				}
-				break;
+		if ($redirect['error'] != false) {
+			echo $redirect['error'];
+		} else {
+			wp_redirect($redirect['url']);
 		}
-
 		die();
 	}
-}
+} );
 
-add_action( 'template_redirect', 'extra_redirect' );
+
+add_filter('wp_nav_menu_objects', function ($items) {
+	foreach ($items as $item) {
+		if ($item->object == 'page') {
+			$slug = get_page_template_slug($item->object_id);
+			if ($slug == 'template-redirect.php') {
+				$redirect = extra_get_redirect ($item->object_id);
+				if ($redirect['error'] == false) {
+					$item->url = $redirect['url'];
+				}
+			}
+		}
+	}
+
+	return $items;
+});

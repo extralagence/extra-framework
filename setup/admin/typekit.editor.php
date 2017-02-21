2c622a8dc9f0d9a7a47c749ed2523e090c31bb53
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2017-02-21 10:31:24 +0100

    :art: Make sure typekit is run on admin only

--- setup/admin/typekit.editor.php
+++ setup/admin/typekit.editor.php
@@ -1,19 +1,23 @@
 <?php
-add_filter("mce_external_plugins", "extra_mce_external_plugins");
-function extra_mce_external_plugins($plugin_array){
-	$extra_typekit_id = apply_filters('extra_admin_typekit_id', '');
-	if (!empty($extra_typekit_id)) {
-		$plugin_array['typekit']  =  EXTRA_URI.'/setup/admin/js/typekit.tinymce.js';
+add_filter( "mce_external_plugins", "extra_mce_external_plugins" );
+function extra_mce_external_plugins( $plugin_array ) {
+	if ( is_admin() ) {
+		$extra_typekit_id = apply_filters( 'extra_admin_typekit_id', '' );
+		if ( ! empty( $extra_typekit_id ) ) {
+			$plugin_array['typekit'] = EXTRA_URI . '/setup/admin/js/typekit.tinymce.js';
+		}
 	}
 
 	return $plugin_array;
 }
 
 
-add_action('before_wp_tiny_mce', 'extra_before_wp_tiny_mce');
+add_action( 'before_wp_tiny_mce', 'extra_before_wp_tiny_mce' );
 function extra_before_wp_tiny_mce() {
-	$extra_typekit_id = apply_filters('extra_admin_typekit_id', '');
-	if (!empty($extra_typekit_id)) {
-		echo "<script>var extra_admin_typekit_id = '".$extra_typekit_id."';</script>";
+	if ( is_admin() ) {
+		$extra_typekit_id = apply_filters( 'extra_admin_typekit_id', '' );
+		if ( ! empty( $extra_typekit_id ) ) {
+			echo "<script>var extra_admin_typekit_id = '" . $extra_typekit_id . "';</script>";
+		}
 	}
 }

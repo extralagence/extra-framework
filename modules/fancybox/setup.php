02f79157b370be952171b04cc026eb5acc438aa0
Vincent Saïsset <vincent.saisset@gmail.com>
2017-03-22 15:41:40 +0100

    ✨ Use only fancybox 3 now

--- modules/fancybox/setup.php
+++ modules/fancybox/setup.php
@@ -14,20 +14,9 @@ function extra_fancybox_init() {
 		return;
 	}
 
-	$extra_fancybox_version = apply_filters( 'extra_fancybox_version', '3' );
-	if ($extra_fancybox_version == '2') {
-
-		wp_enqueue_style( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/v2/source/jquery.fancybox.css', array(), EXTRA_VERSION, 'all' );
-		wp_enqueue_script( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/v2/source/jquery.fancybox.pack.js', array( 'jquery' ), EXTRA_VERSION, true );
-		wp_enqueue_script( 'mousewheel', EXTRA_MODULES_URI . '/fancybox/inc/v2/lib/jquery.mousewheel.pack.js', array( 'fancybox' ), EXTRA_VERSION, true );
-		wp_enqueue_script( 'fancybox.media', EXTRA_MODULES_URI . '/fancybox/inc/v2/source/helpers/jquery.fancybox-media.js', array( 'fancybox' ), EXTRA_VERSION, true );
-		wp_enqueue_script( 'extra.fancybox', EXTRA_MODULES_URI . '/fancybox/js/fancybox.js', array( 'fancybox' ), EXTRA_VERSION, true );
-
-	} else if ($extra_fancybox_version == '3') {
-		wp_enqueue_style( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/jquery.fancybox.min.css', array(), EXTRA_VERSION, 'all' );
-		wp_enqueue_script( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/jquery.fancybox.min.js', array( 'jquery' ), EXTRA_VERSION, true );
-		wp_enqueue_script( 'extra.fancybox', EXTRA_MODULES_URI . '/fancybox/js/fancybox.js', array( 'fancybox' ), EXTRA_VERSION, true );
-	}
+	wp_enqueue_style( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/dist/jquery.fancybox.min.css', array(), EXTRA_VERSION, 'all' );
+	wp_enqueue_script( 'fancybox', EXTRA_MODULES_URI . '/fancybox/inc/dist/jquery.fancybox.min.js', array( 'jquery' ), EXTRA_VERSION, true );
+	wp_enqueue_script( 'extra.fancybox', EXTRA_MODULES_URI . '/fancybox/js/fancybox.js', array( 'fancybox' ), EXTRA_VERSION, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'extra_fancybox_init' );

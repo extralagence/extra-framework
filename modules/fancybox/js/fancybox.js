aca79518800b44b7919a08efe202d7f920ea4a11
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2017-04-25 15:19:25 +0200

    :bug: Make sure that fanybox isn't initialized more than one time

--- modules/fancybox/js/fancybox.js
+++ modules/fancybox/js/fancybox.js
@@ -8,10 +8,13 @@ $window.on('extra:fancybox:init', function (event, $parent) {
 	}
 });
 function extraInitFancybox($parent) {
+	if ($parent.hasClass('extra-fancybox-initialised')) {
+		return;
+	}
 	// DEFAULT OPTIONS
 	var $metaTitle = $('meta[name="extra:fancybox_title"]'),
 		extraFancyboxDefaultOptions = {
-			opacity : false,
+			opacity: false,
 
 			// Should display toolbars
 			infobar : true,
@@ -19,31 +22,31 @@ function extraInitFancybox($parent) {
 			smallBtn: false,
 
 			// What buttons should appear in the toolbar
-			slideShow  : false,
-			fullScreen : false,
-			thumbs     : false,
-			closeBtn   : true,
+			slideShow : false,
+			fullScreen: false,
+			thumbs    : false,
+			closeBtn  : true,
 
-			errorTpl : '<div class="fancybox-error"><p>' + extra_fancybox_options.messages.error + '<p></div>',
-			margin      : [140, 90, 120, 90],
-			baseTpl     : 	'<div class="fancybox-container" role="dialog" tabindex="-1">' +
-								'<div class="fancybox-bg"></div>' +
-								'<div class="fancybox-controls">' +
-									'<div class="fancybox-infobar__body">' +
-										'<h2 class="extra-fancybox-title">' + $metaTitle.attr('content') + '</h2>' +
-										'<div class="extra-fancybox-counter"><div class="inner"><div class="text">' + extra_fancybox_options.messages.count + '</div></div></div>' +
-									'</div>' +
-									'<div class="fancybox-buttons">' +
-										'<button data-fancybox-previous class="extra-fancybox-nav extra-fancybox-prev"><span class="icon-arrow"></span></button>' +
-										'<button data-fancybox-next class="extra-fancybox-nav extra-fancybox-next" ><span class="icon-arrow"></span></button>' +
-										'<button data-fancybox-close class="extra-fancybox-nav close-button"><span class="icon-close"></span></button>' +
-									'</div>' +
-								'</div>' +
-								'<div class="fancybox-slider-wrap">' +
-									'<div class="fancybox-slider"></div>' +
-								'</div>' +
-								'<div class="fancybox-caption-wrap"><div class="fancybox-caption"></div></div>' +
-							'</div>'
+			errorTpl: '<div class="fancybox-error"><p>' + extra_fancybox_options.messages.error + '<p></div>',
+			margin  : [140, 90, 120, 90],
+			baseTpl : '<div class="fancybox-container" role="dialog" tabindex="-1">' +
+			'<div class="fancybox-bg"></div>' +
+			'<div class="fancybox-controls">' +
+			'<div class="fancybox-infobar__body">' +
+			'<h2 class="extra-fancybox-title">' + $metaTitle.attr('content') + '</h2>' +
+			'<div class="extra-fancybox-counter"><div class="inner"><div class="text">' + extra_fancybox_options.messages.count + '</div></div></div>' +
+			'</div>' +
+			'<div class="fancybox-buttons">' +
+			'<button data-fancybox-previous class="extra-fancybox-nav extra-fancybox-prev"><span class="icon-arrow"></span></button>' +
+			'<button data-fancybox-next class="extra-fancybox-nav extra-fancybox-next" ><span class="icon-arrow"></span></button>' +
+			'<button data-fancybox-close class="extra-fancybox-nav close-button"><span class="icon-close"></span></button>' +
+			'</div>' +
+			'</div>' +
+			'<div class="fancybox-slider-wrap">' +
+			'<div class="fancybox-slider"></div>' +
+			'</div>' +
+			'<div class="fancybox-caption-wrap"><div class="fancybox-caption"></div></div>' +
+			'</div>'
 		},
 
 		// GET ALL ELEMENTS
@@ -67,7 +70,7 @@ function extraInitFancybox($parent) {
 		}
 
 		var $caption = ($this.next().is('.wp-caption-text')) ? $this.next() : $this.find('.wp-caption-text');
-		if($caption.length > 0) {
+		if ($caption.length > 0) {
 			$this.data('caption', $caption.html());
 		}
 	});
@@ -91,4 +94,6 @@ function extraInitFancybox($parent) {
 			});
 		});
 	}
+
+	$parent.addClass('extra-fancybox-initialised');
 }

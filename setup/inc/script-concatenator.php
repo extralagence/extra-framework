3f8a99090805c17ce7708ba6c0a94a8644acf0bb
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2017-05-02 20:37:18 +0200

    :bug: Prevent macos files from script concatenator

--- setup/inc/script-concatenator.php
+++ setup/inc/script-concatenator.php
@@ -7,10 +7,13 @@ function extra_script_concatenator( $folder_path, $file_path ) {
 	$folder_files       = scandir( $folder_path );
 	$functions_excluded = array(
 		'.',
-		'..'
+		'..',
+		'Icon',
+		'.DS_Store'
 	);
 
 	$file_content = '';
+//	var_dump($folder_files);
 	foreach ( $folder_files as $folder_file ) {
 		if ( is_file( $folder_path . '/' . $folder_file ) && ! in_array( $folder_file, $functions_excluded ) ) {
 			$file_content .= file_get_contents( $folder_path . '/' . $folder_file );
